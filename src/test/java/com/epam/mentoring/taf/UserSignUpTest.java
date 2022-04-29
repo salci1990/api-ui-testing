663489c26a9a11954e9d2f04bf2a08c4575d6870
Artur_Kowalski <artur_kowalski@epam.com>
2022-04-29 16:24:34 +0200

    Changes in api tests.

--- src/test/java/com/epam/mentoring/taf/UserSignUpTest.java
+++ src/test/java/com/epam/mentoring/taf/UserSignUpTest.java
@@ -1,12 +1,10 @@
 package com.epam.mentoring.taf;
 
-import io.restassured.http.ContentType;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import com.epam.mentoring.taf.api.endpoints.SignUpApi;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItem;
 
 public class UserSignUpTest extends AbstractTest {
@@ -14,6 +12,13 @@ public class UserSignUpTest extends AbstractTest {
     private final String username = "Test User";
     private final String email = "test_user@example.com";
     private final String password = "test_password";
+
+    private SignUpApi signUpApi;
+
+    @BeforeClass
+    public void setupTest() {
+        signUpApi = new SignUpApi();
+    }
 
     @Test(groups = "UITests")
     public void uiSignUpVerificationTest() {
@@ -45,44 +50,34 @@ public class UserSignUpTest extends AbstractTest {
     }
 
     @Test(groups = "APITest")
-    public void apiVerification() {
+    public void apiSuccesfullSignUpTest() {
         int uniqueId = (int) (Math.random() * 100);
         String username = this.username + uniqueId;
         String email = this.email.replace("@", "." + uniqueId + "@");
 
-        given().baseUri(API_URL)
-                .when()
-                .contentType(ContentType.JSON)
-                .body(String.format("{\"user\":{\"email\":\"%s\",\"password\":\"%s\",\"username\":\"%s\"}}", email, password, username))
-                .post("/api/users")
+        signUpApi
+                .signUpUser(String.format("{\"user\":{\"email\":\"%s\",\"password\":\"%s\",\"username\":\"%s\"}}", email, password, username))
                 .then()
                 .statusCode(200);
     }
 
     @Test(groups = "APITest")
-    public void apiAlreadyRegisteredVerification() {
-        given().baseUri(API_URL)
-                .when()
-                .contentType(ContentType.JSON)
-                .body(String.format("{\"user\":{\"email\":\"%s\",\"password\":\"%s\",\"username\":\"%s\"}}", email, password, username))
-                .post("/api/users")
+    public void apiAlreadyRegisteredUserTest() {
+
+        signUpApi
+                .signUpUser(String.format("{\"user\":{\"email\":\"%s\",\"password\":\"%s\",\"username\":\"%s\"}}", email, password, username))
                 .then()
                 .statusCode(422)
                 .body("errors.email", hasItem("has already been taken"));
     }
 
     @Test(enabled = false, groups = "APITest", description = "api Wrong Email during signUp test")
-    public void apiWrongEmailVerification() {
-        given().log().all()
-                .baseUri(API_URL)
-                .when()
-                .contentType(ContentType.JSON)
-                .body(String.format("{\"user\":{\"email\":\"%s\",\"password\":\"%s\",\"username\":\"%s\"}}", "wrong_email", password, username))
-                .log().all()
-                .post("/api/users")
+    public void apiWrongEmailVerificationTest() {
+
+        signUpApi
+                .signUpUser(String.format("{\"user\":{\"email\":\"%s\",\"password\":\"%s\",\"username\":\"%s\"}}", "wrong_email", password, username))
                 .then()
                 .statusCode(422)
                 .body("errors.email", hasItem("is invalid"));
     }
-
 }
